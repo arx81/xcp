1d8feef826de1cf051720b9b6b9fd47f67c7dfdc
arx81 <43811517+arx81@users.noreply.github.com>
2018-12-07 13:19:06 +0100

    Create cx.sh

---
+++ cx.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+host=$1
+nomeutente=$2
+password=$3
+
+echo ${nomeutente}
+echo ${password}
+echo ${host}
+
+
+
+if [ -d /etc/enigma2/xc ]; then
+    rm -r /etc/enigma2/xc
+else
+    echo "  non esiste la cartella ";
+fi
+
+    
+
+wget -O /tmp/xcplugin.ipk "http://lululla.altervista.org/upload/index.php?dir=files/&file=enigma2-plugin-extensions-xcplugin-lite_1.0_all.ipk" && chmod 777 /tmp/xcplugin.ipk && opkg install /tmp/xcplugin.ipk && wget -O /etc/enigma2/iptv.sh "http://"${host}"/get.php?username="${nomeutente}"&password="${password}"&type=enigma22_script&output=ts" && /etc/enigma2/iptv.sh && echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<items>
+<plugin_version>1.0</plugin_version>                              
+<xtream_e2portal_url><![CDATA[http://"${host}"/enigma2.php]]></xtream_e2portal_url>
+<!--Edit Username and Password with your login:password -->
+<username>"${nomeutente}"</username>
+<password>"${password}"</password>
+
+</items>" >> /etc/enigma2/xc/xc_e2_plugin.xml && chmod 777 /etc/enigma2/xc/xc_e2_plugin.xml
+
+exit
+
