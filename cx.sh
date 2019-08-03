844fb6528aa92c40454610f882359ffad0801962
arx81 <43811517+arx81@users.noreply.github.com>
2019-08-03 13:14:31 +0200

    Update cx.sh

--- cx.sh
+++ cx.sh
@@ -17,7 +17,7 @@ fi
 
     
 
-wget -O /tmp/xcplugin.ipk "http://download1151.mediafire.com/gpg85piou8rg/fy8wzapo7881ekr/enigma2-plugin-extensions-xcplugin-lite_1.0_all.ipk" && chmod 777 /tmp/xcplugin.ipk && opkg install /tmp/xcplugin.ipk && wget -O /etc/enigma2/iptv.sh "http://"${host}"/get.php?username="${nomeutente}"&password="${password}"&type=enigma22_script&output=ts" && /etc/enigma2/iptv.sh && echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+wget -O /tmp/xcplugin.ipk "http://download855.mediafire.com/oc36hb2jfkzg/bynbri69r9bo1g8/enigma2-plugin-extensions-xcplugin-lite_2.1_all.ipk" && chmod 777 /tmp/xcplugin.ipk && opkg install /tmp/xcplugin.ipk && wget -O /etc/enigma2/iptv.sh "http://"${host}"/get.php?username="${nomeutente}"&password="${password}"&type=enigma22_script&output=ts" && /etc/enigma2/iptv.sh && echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <items>
 <plugin_version>1.0</plugin_version>                              
 <xtream_e2portal_url><![CDATA[http://"${host}"/enigma2.php]]></xtream_e2portal_url>
