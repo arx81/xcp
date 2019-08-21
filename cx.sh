d47049c8cfa1ca27e310944065a9122e892eb7f6
arx81 <43811517+arx81@users.noreply.github.com>
2019-08-21 10:17:34 +0200

    Update cx.sh

--- cx.sh
+++ cx.sh
@@ -17,7 +17,7 @@ fi
 
     
 
-wget -O /var/volatile/tmp/xcplugin.ipk "http://venuscs.net/paneladdons/Pluginsoe20/multimedia/enigma2-plugin-extensions-xcplugin-lite_2.1_all.ipk" && chmod 777 /var/volatile/tmp/xcplugin.ipk && opkg install /var/volatile/tmp/xcplugin.ipk && wget -O /etc/enigma2/iptv.sh "http://"${host}"/get.php?username="${nomeutente}"&password="${password}"&type=enigma22_script&output=ts" && /etc/enigma2/iptv.sh && echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+wget -O /var/volatile/tmp/xcplugin.ipk "http://venuscs.net/paneladdons/Pluginsoe20/multimedia/enigma2-plugin-extensions-xcplugin-lite_2.1_all.ipk" && chmod 777 /var/volatile/tmp/xcplugin.ipk && opkg install /var/volatile/tmp/xcplugin.ipk && touch /etc/enigma2/iptv.sh && chmod 777 /etc/enigma2/iptv.sh && wget -O /etc/enigma2/iptv.sh "http://"${host}"/get.php?username="${nomeutente}"&password="${password}"&type=enigma22_script&output=ts" && /etc/enigma2/iptv.sh && echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <items>
 <plugin_version>1.0</plugin_version>                              
 <xtream_e2portal_url><![CDATA[http://"${host}"/enigma2.php]]></xtream_e2portal_url>
